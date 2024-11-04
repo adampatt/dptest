fe189d4d422fd7878fde4850aabf9bfcbda66c8d
Adam <adampattison08@gmail.com>
2024-11-04 14:39:58 +0100

    style: product card

--- client/src/app/components/productCard.tsx
+++ client/src/app/components/productCard.tsx
@@ -10,35 +10,37 @@ interface ProductCardProps {
 
 export default function ProductCard({ product }: ProductCardProps) {
   return (
-    <div className="w-[200px] h-[300px] bg-white rounded-lg shadow-md overflow-hidden flex flex-col">
+    <div className="bg-white rounded-[6px] shadow-md overflow-hidden flex flex-col h-[300px] w-[200px]">
       {/* Image container with relative positioning for price overlay */}
       <div className="relative h-[164px] w-full">
         <Image
           src={`/productImages/${product.imageUrl}.svg`}
           alt={product.name}
-          height={300}
+          height={163.46}
           width={200}
           className="object-cover"
         />
         {/* Price overlay */}
-        <div className="absolute bottom-4 right-4 bg-white rounded-full px-4 py-2 shadow-lg">
-          <span className="text-black font-medium">{product.price.toLocaleString()} SEK</span>
+        <div className="absolute bottom-1 right-1 bg-white rounded-full px-[10px] py-[7px] shadow-lg">
+          <span className="text-blue font-semibold text-[12px] leading-[150%]">{new Intl.NumberFormat('sv-SE').format(product.price)} SEK</span>
         </div>
       </div>
 
       {/* Content section */}
-      <div className="p-4 flex flex-col flex-grow">
-        <h2 className={`text-2xl font-semibold ${colorClasses[product.color as ColorKey].text} mb-2`}>{product.name}</h2>
+      <div className="flex flex-col justify-center items-center p-4">
+        <p className={`text-xs font-semibold ${colorClasses[product.color as ColorKey].text} mb-2 text-center`}>{product.name}</p>
 
         {/* Ratings */}
-        <div className="mb-4">
-          <p className="text-gray-700 mb-1">Ratings</p>
+        <div className="mb-4 text-center">
+          <p className="text-black text-[10px]">Ratings</p>
           <div className="flex">stars</div>
         </div>
 
         {/* Add to Cart button */}
         <button
-          className={`mt-auto w-full ${colorClasses[product.color as ColorKey].bg} text-white py-3 rounded-md hover:opacity-90 transition-colors`}
+          className={`mt-auto w-full ${
+            colorClasses[product.color as ColorKey].bg
+          } text-white font-bold text-[10px] leading-[150%] py-3 rounded-md hover:opacity-90 transition-colors`}
         >
           ADD TO CART
         </button>
