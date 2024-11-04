e634815b44bf31fdeafc0b02de47598b9764fc81
Adam <adampattison08@gmail.com>
2024-11-04 13:13:05 +0100

    feat: create getAllProducts schema, implement in get.

--- server/src/routes/product.ts
+++ server/src/routes/product.ts
@@ -1,8 +1,29 @@
 import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
 import products from '../../data/products.json';
 
+const getAllProductsSchema = {
+    response: {
+      200: {
+        type: 'array',
+        items: {
+          type: 'object',
+          properties: {
+            name: { type: 'string' },
+            rating: { type: 'number' },
+            price: { type: 'number' },
+            imageUrl: { type: 'string' },
+            color: { type: 'string' },
+          },
+          required: ['name', 'rating', 'price', 'imageUrl', 'color'],
+        },
+      },
+    },
+  };
+
 export default async function productRoutes(fastify: FastifyInstance) {
-    fastify.get('/products',
+    fastify.get('/products', {
+        schema: getAllProductsSchema,
+    },
     async (request: FastifyRequest, reply: FastifyReply) => {
       reply.send(products);
     });
