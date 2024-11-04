f9926c5ff5b3b71fd98aeb4e51cee824a1e52305
Adam <adampattison08@gmail.com>
2024-11-04 14:39:44 +0100

    style: use inter as default typography throughout app

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,16 +1,10 @@
 import type { Metadata } from 'next';
-import localFont from 'next/font/local';
+import { Inter } from 'next/font/google';
 import './globals.css';
 
-const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
-  variable: '--font-geist-sans',
-  weight: '100 900',
-});
-const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
-  variable: '--font-geist-mono',
-  weight: '100 900',
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
 });
 
 export const metadata: Metadata = {
@@ -24,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased bg-off-white min-h-screen`}>{children}</body>
+      <body className={`${inter.variable} antialiased bg-off-white min-h-screen`}>{children}</body>
     </html>
   );
 }
